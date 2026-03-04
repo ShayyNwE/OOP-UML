e16db671506ba09a487258b6e4652d91bf89d617
Tom Julliat <tom.julliat@next-u.fr>
2026-03-04 10:55:13 +0100

    Création class human

---
+++ human_class.php
@@ -0,0 +1,27 @@
+<?php
+
+class Human {
+    public $nom;
+    public $prenom;
+    public $age;
+    public $taille;
+    public $sexe;
+    public $poids;
+
+    public function __construct($nom, $prenom, $age, $taille, $sexe, $poids) {
+        $this->name = $nom;
+        $this->prenom = $prenom;
+        $this->age = $age;
+        $this->taille = $taille;
+        $this->sexe = $sexe;
+        $this->poids = $poids;
+    }
+
+    public function marcher() {
+        return "Je marche";
+    }
+
+    public function parler() {
+        return "Je parle";
+    }
+}
